147155758ad1f5c01822f47f603f7c013fefb584
abrahamjimenez <abraham.jimenez.romo@gmail.com>
2022-09-13 21:58:33 +0000

    simplfied code

--- W01/scripts/script.js
+++ W01/scripts/script.js
@@ -1,11 +1,1 @@
-var today = new Date();
-const day = today.getDate();
-const month = today.getMonth();
-const year = today.getFullYear();
-const hour = today.getHours();
-const minute = today.getMinutes();
-const seconds = today.getSeconds();
-
-var today = `${month}/${day}/${year} ${hour}:${minute}:${seconds}`;
-
-document.querySelector(".date").innerHTML = today;
+document.querySelector(".date").innerHTML = document.lastModified;
