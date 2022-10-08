4fec2faceac09c84e4b7f2b69ad27187bdf197eb
abrahamjimenez <abraham.jimenez.romo@gmail.com>
2022-10-08 19:21:02 +0000

    added header

---
+++ chamber/scripts/script.js
@@ -0,0 +1,35 @@
+const now = new Date();
+
+var days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+var months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+var day = days[now.getDay()];
+var month = months[now.getMonth()];
+const weekday = now.getDay();
+const year = now.getFullYear();
+
+document.querySelector(".weekday").innerHTML = day;
+document.querySelector(".day").innerHTML = weekday;
+document.querySelector(".month").innerHTML = month;
+document.querySelector(".year").innerHTML = year;
