f2525697e0a409ce5bf0d77503af8a284a1212ad
abrahamjimenez <abraham.jimenez.romo@gmail.com>
2022-11-01 21:27:44 +0000

    updated

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -65,3 +65,16 @@ function alertFunction() {
 }
 
 window.addEventListener("load", alertFunction);
+
+// Lazy Loading
+let imagesToLoad = document.querySelectorAll("img[data-src");
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+imagesToLoad.forEach((img) => {
+  loadImages(img);
+});
